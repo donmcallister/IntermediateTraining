31d5102dc5cb0970ca92723ce0320c09f3e67a99
Don <mcallisterda@gmail.com>
2019-09-18 11:41:44 -0400

    custom company cell

--- IntermediateTraining/Companies/CompaniesController.swift
+++ IntermediateTraining/Companies/CompaniesController.swift
@@ -49,7 +49,7 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
         tableView.separatorColor = .white
         tableView.tableFooterView = UIView() // blank UIView
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.register(CompanyCell.self, forCellReuseIdentifier: "cellId")
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
         
@@ -143,33 +143,16 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
         return 50
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! CompanyCell
         let company = companies[indexPath.row]
-        
-        if let name = company.name, let founded = company.founded {
-            
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "MMM dd, yyyy"
-            let foundedDateString = dateFormatter.string(from: founded)
-        
-            let dateString = "\(name) - Founded: \(foundedDateString)"
-            cell.textLabel?.text = dateString
-        } else {
-            cell.textLabel?.text = company.name
-        }
-        
-        cell.textLabel?.textColor = .white
-        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        cell.backgroundColor = .tealColor
-        
-        cell.imageView?.image = #imageLiteral(resourceName: "select_photo_empty")
-        
-        if let imageData = company.imageData {
-            cell.imageView?.image = UIImage(data: imageData)
-        }
-        
+        cell.company = company
+    
         return cell 
         
     }
