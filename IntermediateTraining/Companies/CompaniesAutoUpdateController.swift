970c55a1e143727081ff8469c2d2394241f9b9eb
Don <mcallisterda@gmail.com>
2019-09-20 12:12:35 -0400

    Auto UI Updates with NSFetchedResultsController, delegate update to push the new autoupdate controller to test drive NSFetchedResultsController

---
+++ IntermediateTraining/Companies/CompaniesAutoUpdateController.swift
@@ -0,0 +1,153 @@
+//
+//  CompaniesAutoUpdateController.swift
+//  IntermediateTraining
+//
+//  Created by Donald McAllister on 9/20/19.
+//  Copyright © 2019 Donald McAllister. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CompaniesAutoUpdateController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    //warning: this code is a beast
+    lazy var fetchedResultsController: NSFetchedResultsController<Company> = {
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        let request: NSFetchRequest<Company> = Company.fetchRequest()
+        request.sortDescriptors = [
+            NSSortDescriptor(key: "name", ascending: true)
+        ]
+        let frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: "name", cacheName: nil)
+        
+        //listen to when things happen in frc with core data changes
+        frc.delegate = self //need to change frc to lazy var as above since = self here
+        
+        do {
+            try frc.performFetch()
+        } catch let err {
+            print(err)
+        }
+        
+        return frc
+    }()
+    
+    // called when any updates occur:
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .move:
+            break
+        case .update:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .fade)
+        case .move:
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    @objc private func handleAdd() {
+        print("let's add company called BMW")
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        let company = Company(context: context)
+        company.name = "BMW"
+        
+        try? context.save()
+    }
+    
+    @objc private func handleDelete() {
+        let request: NSFetchRequest<Company> = Company.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "name CONTAINS %@", "B")
+        
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        let companiesWithB = try? context.fetch(request)
+        
+        companiesWithB?.forEach({ (company) in
+            context.delete(company)
+        })
+        
+        try? context.save()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Company Auto Updates"
+        
+        navigationItem.leftBarButtonItems = [
+            UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(handleAdd)),
+            UIBarButtonItem(title: "Delete", style: .plain, target: self, action: #selector(handleDelete))
+        ]
+        
+        tableView.register(CompanyCell.self, forCellReuseIdentifier: cellId)
+        
+        tableView.backgroundColor = UIColor.darkBlue
+        
+        fetchedResultsController.fetchedObjects?.forEach({ (company) in
+            print(company.name ?? "")
+        })
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.sections![section].numberOfObjects
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = IndentedLabel()
+        label.text = fetchedResultsController.sectionIndexTitles[section]
+        label.backgroundColor = UIColor.lightBlue
+        return label
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, sectionIndexTitleForSectionName sectionName: String) -> String? {
+        return sectionName
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    let cellId = "cellId"
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! CompanyCell
+        
+        let company = fetchedResultsController.object(at: indexPath)
+      //  cell.textLabel?.text = company.name
+        cell.company = company
+        
+        return cell
+    }
+}
